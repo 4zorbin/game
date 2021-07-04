e2c019229bdf2eda3fdd4f9ced2e7cea71773174
4zorbin <adwell83@gmail.com>
2021-07-04 20:32:30 +0300

    add btn nwe card fix js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
 let firstcard = 10
-let secondcard = 12
+let secondcard = 6
 let sum = firstcard + secondcard
 let blackJack = false
 let message = ""
 
-function startgame(){
+let messageEl = document.getElementById('messege-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame(){
    if (sum <= 20){
-        message = "teke a card"
+        message = "Teke a card"
         console.log(message)
     }else if(sum === 21){
         message = 'Got a Blackjak!!!'
@@ -16,4 +25,14 @@ function startgame(){
         message = "Your out!!!"
         console.log(message)
     }
+    messageEl.textContent = message
+    sumEl.textContent = 'Sum: ' + sum
+    cardsEl.textContent = "Cards: " + firstcard + " " + secondcard
+}
+
+function newcard(){
+    console.log('New card from the deck!')
+    let card = 5
+    sum += card
+    renderGame()
 }
